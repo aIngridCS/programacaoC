c328329477ecb4a6d32baab05f541d42a900e34d
Ingrid Carvalho <ingridcs2895@gmail.com>
2026-04-08 10:59:47 +0000

    2

---
+++ N1_NC4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+    int idade = 31;
+    int quantidade = 1;
+    float altura = 1.58;
+    double peso = 60.7;
+    char letra = 'A';
+    char nome[20] = "ingrid";
+
+
+    printf("O nome dela é: %s\n", nome);
+    printf("A idade é: %d\n", idade);
+    printf("A altura é: %.2f\n", altura);
+    printf("O peso é: %.2f\n",peso);
+
+
+
+
+}
